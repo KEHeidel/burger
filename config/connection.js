b0ad2f52fa3205a3e0d89a12be252fb6e7c518c2
Kortnie Heidel <kortnie.evans@gmail.com>
2020-05-11 18:00:08 -0400

    test connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
     host: "us-cdbr-east-06.cleardb.net",
     port: 3306,
     user: "bb2e168b6b2b0e",
